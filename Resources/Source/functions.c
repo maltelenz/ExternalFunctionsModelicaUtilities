a9abb258f829559e72bb7ce832c488d7a63bae33
Malte Lenz <maltel@wolfram.com>
2025-10-28 13:57:10 +0100

    Print newlines.

--- Resources/Source/functions.c
+++ Resources/Source/functions.c
@@ -15,18 +15,18 @@ double compute(const char *name, double x)
 {
   double y;
   char str[100];
- 
+
   if (!strcmp(name, "foobar"))
     {
 	  y = 1 + x*x;
 	  if (y > 10.0)
 	  {
-	    snprintf(str, 100, "Value of x = %f could be too high", x);
+	    snprintf(str, 100, "Value of x = %f could be too high\n", x);
 		ModelicaWarning(str);
 	  }
 	  if (y > 17.0)
 	  {
-	    snprintf(str, 100, "Value of x = %f is too high", x);
+	    snprintf(str, 100, "Value of x = %f is too high\n", x);
 		ModelicaError(str);
 	  }
 	  return y;
